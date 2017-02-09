5704da26f95d6f15fbdafd7f623918fcf4b4ad2c
klausbohm <klaus.bohm@gmx.ch>
2017-02-09 17:50:16 +0100

    upload first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportshope32::Application.config.secret_token = 'ae0a3e8a3d7e9bcc530cc904c630f197084ad106fd000d7b46131b752c19950e924b796f91bbc9a170780f8c8aef17d5d76767b173e7dafb675b4c48008a28ed'
